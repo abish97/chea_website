8d0eb1e6f6b35dbb9228d27058cf44fb762704f2
abish97 <kumarabhishek22101997@gmail.com>
2019-01-13 18:21:04 +0530

    changesinnew

--- blog/static/js/team.js
+++ blog/static/js/team.js
@@ -10,8 +10,8 @@ var revert1 = function(){
   $('.box1 .el').css("opacity", "1.0");
   $('.box1').css("background", "none");
   $('.box1 .number').css("opacity", "1.0");
-  $('.box1 .name').css("color", "black");
-  $('.box1 .position').css("color", "black");
+  $('.box1 .name').css("color", "white");
+  $('.box1 .position').css("color", "white");
 }
 
 var change2 = function(){
@@ -28,8 +28,8 @@ var revert2 = function(){
   $('.box2').css("background", "none");
   $('.box2 .number').css("opacity", "1.0");
 
-  $('.box2 .name').css("color", "black");
-  $('.box2 .position').css("color", "black");
+  $('.box2 .name').css("color", "white");
+  $('.box2 .position').css("color", "white");
 }
 var change3 = function(){
   $('.box3 .el').css("opacity", "0.0");
@@ -45,8 +45,8 @@ var revert3 = function(){
   $('.box3').css("background", "none");
   $('.box3 .number').css("opacity", "1.0");
 
-  $('.box3 .name').css("color", "black");
-  $('.box3 .position').css("color", "black");
+  $('.box3 .name').css("color", "white");
+  $('.box3 .position').css("color", "white");
 }
 var change4 = function(){
   $('.box4 .el').css("opacity", "0.0");
@@ -63,8 +63,8 @@ var revert4 = function(){
   $('.box4 .number').css("opacity", "1.0");
   $('.box4').css("background", "url(https://raw.githubusercontent.com/abish97/chea_website/master/blog/static/images/6.jpeg)");
   $('.box4').css("opacity","0.3");
-  $('.box4 .name').css("color", "black");
-  $('.box4 .position').css("color", "black");
+  $('.box4 .name').css("color", "white");
+  $('.box4 .position').css("color", "white");
 }
 var change5 = function(){
   $('.box5 .el').css("opacity", "0.0");
@@ -82,8 +82,8 @@ var revert5 = function(){
   $('.box5').css("background", "none");
   $('.box5 .number').css("opacity", "1.0");
 
-  $('.box5 .name').css("color", "black");
-  $('.box5 .position').css("color", "black");
+  $('.box5 .name').css("color", "white");
+  $('.box5 .position').css("color", "white");
 }
 var change6 = function(){
   $('.box6 .el').css("opacity", "0.0");
@@ -99,8 +99,8 @@ var revert6 = function(){
   $('.box6').css("background", "none");
   $('.box6 .number').css("opacity", "1.0");
 
-  $('.box6 .name').css("color", "black");
-  $('.box6 .position').css("color", "black");
+  $('.box6 .name').css("color", "white");
+  $('.box6 .position').css("color", "white");
 }
 var change7 = function(){
   $('.box7 .el').css("opacity", "0.0");
@@ -116,8 +116,8 @@ var revert7 = function(){
   $('.box7').css("background", "none");
   $('.box7 .number').css("opacity", "1.0");
 
-  $('.box7 .name').css("color", "black");
-  $('.box7 .position').css("color", "black");
+  $('.box7 .name').css("color", "white");
+  $('.box7 .position').css("color", "white");
 }
 var change8 = function(){
   $('.box8 .el').css("opacity", "0.0");
@@ -133,8 +133,8 @@ var revert8 = function(){
   $('.box8').css("background", "none");
   $('.box8 .number').css("opacity", "1.0");
 
-  $('.box8 .name').css("color", "black");
-  $('.box8 .position').css("color", "black");
+  $('.box8 .name').css("color", "white");
+  $('.box8 .position').css("color", "white");
 }
 var change9 = function(){
   $('.box9 .el').css("opacity", "0.0");
@@ -150,8 +150,8 @@ var revert9 = function(){
   $('.box9').css("background", "none");
   $('.box9 .number').css("opacity", "1.0");
 
-  $('.box9 .name').css("color", "black");
-  $('.box9 .position').css("color", "black");
+  $('.box9 .name').css("color", "white");
+  $('.box9 .position').css("color", "white");
 }
 var change10 = function(){
   $('.box10 .el').css("opacity", "0.0");
@@ -167,8 +167,8 @@ var revert10 = function(){
   $('.box10').css("background", "none");
   $('.box10 .number').css("opacity", "1.0");
 
-  $('.box10 .name').css("color", "black");
-  $('.box10 .position').css("color", "black");
+  $('.box10 .name').css("color", "white");
+  $('.box10 .position').css("color", "white");
 }
 var change11 = function(){
   $('.box11 .el').css("opacity", "0.0");
@@ -184,8 +184,8 @@ var revert11 = function(){
   $('.box11').css("background", "none");
   $('.box11 .number').css("opacity", "1.0");
 
-  $('.box11 .name').css("color", "black");
-  $('.box11 .position').css("color", "black");
+  $('.box11 .name').css("color", "white");
+  $('.box11 .position').css("color", "white");
 }
 var change12 = function(){
   $('.box12 .el').css("opacity", "0.0");
@@ -201,8 +201,8 @@ var revert12 = function(){
   $('.box12').css("background", "none");
   $('.box12 .number').css("opacity", "1.0");
 
-  $('.box12 .name').css("color", "black");
-  $('.box12 .position').css("color", "black");
+  $('.box12 .name').css("color", "white");
+  $('.box12 .position').css("color", "white");
 }
 var change13 = function(){
   $('.box13 .el').css("opacity", "0.0");
@@ -218,8 +218,8 @@ var revert13 = function(){
   $('.box13').css("background", "none");
   $('.box13 .number').css("opacity", "1.0");
 
-  $('.box13 .name').css("color", "black");
-  $('.box13 .position').css("color", "black");
+  $('.box13 .name').css("color", "white");
+  $('.box13 .position').css("color", "white");
 }
 var change14 = function(){
   $('.box14 .el').css("opacity", "0.0");
@@ -235,8 +235,8 @@ var revert14 = function(){
   $('.box14').css("background", "none");
   $('.box14 .number').css("opacity", "1.0");
 
-  $('.box14 .name').css("color", "black");
-  $('.box14 .position').css("color", "black");
+  $('.box14 .name').css("color", "white");
+  $('.box14 .position').css("color", "white");
 }
 var change15 = function(){
   $('.box15 .el').css("opacity", "0.0");
@@ -252,8 +252,8 @@ var revert15 = function(){
   $('.box15').css("background", "none");
   $('.box15 .number').css("opacity", "1.0");
 
-  $('.box15 .name').css("color", "black");
-  $('.box15 .position').css("color", "black");
+  $('.box15 .name').css("color", "white");
+  $('.box15 .position').css("color", "white");
 }
 var change16 = function(){
   $('.box16 .el').css("opacity", "0.0");
@@ -270,8 +270,8 @@ var revert16 = function(){
   $('.box16').css("background", "none");
   $('.box16 .number').css("opacity", "1.0");
 
-  $('.box16 .name').css("color", "black");
-  $('.box16 .position').css("color", "black");
+  $('.box16 .name').css("color", "white");
+  $('.box16 .position').css("color", "white");
 }
 var change17 = function(){
   $('.box17 .el').css("opacity", "0.0");
@@ -287,8 +287,8 @@ var revert17 = function(){
   $('.box17').css("background", "none");
   $('.box17 .number').css("opacity", "1.0");
 
-  $('.box17 .name').css("color", "black");
-  $('.box17 .position').css("color", "black");
+  $('.box17 .name').css("color", "white");
+  $('.box17 .position').css("color", "white");
 }
 var change18 = function(){
   $('.box18 .el').css("opacity", "0.0");
@@ -304,8 +304,8 @@ var revert18 = function(){
   $('.box18').css("background", "none");
   $('.box18 .number').css("opacity", "1.0");
 
-  $('.box18 .name').css("color", "black");
-  $('.box18 .position').css("color", "black");
+  $('.box18 .name').css("color", "white");
+  $('.box18 .position').css("color", "white");
 }
 var change19 = function(){
   $('.box19 .el').css("opacity", "0.0");
@@ -321,8 +321,8 @@ var revert19 = function(){
   $('.box19').css("background", "none");
   $('.box19 .number').css("opacity", "1.0");
 
-  $('.box19 .name').css("color", "black");
-  $('.box19 .position').css("color", "black");
+  $('.box19 .name').css("color", "white");
+  $('.box19 .position').css("color", "white");
 }
 var change20 = function(){
   $('.box20 .el').css("opacity", "0.0");
@@ -338,8 +338,8 @@ var revert20 = function(){
   $('.box20').css("background", "none");
   $('.box20 .number').css("opacity", "1.0");
 
-  $('.box20 .name').css("color", "black");
-  $('.box20 .position').css("color", "black");
+  $('.box20 .name').css("color", "white");
+  $('.box20 .position').css("color", "white");
 }
 var change21 = function(){
   $('.box21 .el').css("opacity", "0.0");
@@ -353,8 +353,8 @@ var revert21 = function(){
   $('.box21 .el').css("opacity", "1.0");
   $('.box21').css("background", "none");
   $('.box21 .number').css("opacity", "1.0");
-  $('.box21 .name').css("color", "black");
-  $('.box21 .position').css("color", "black");
+  $('.box21 .name').css("color", "white");
+  $('.box21 .position').css("color", "white");
 }
 var change22 = function(){
   $('.box22 .el').css("opacity", "0.0");
@@ -368,8 +368,8 @@ var revert22 = function(){
   $('.box22 .el').css("opacity", "1.0");
   $('.box22').css("background", "none");
   $('.box22 .number').css("opacity", "1.0");
-  $('.box22 .name').css("color", "black");
-  $('.box22 .position').css("color", "black");
+  $('.box22 .name').css("color", "white");
+  $('.box22 .position').css("color", "white");
 }
 var change23 = function(){
   $('.box23 .el').css("opacity", "0.0");
@@ -383,8 +383,8 @@ var revert23 = function(){
   $('.box23 .el').css("opacity", "1.0");
   $('.box23').css("background", "none");
   $('.box23 .number').css("opacity", "1.0");
-  $('.box23 .name').css("color", "black");
-  $('.box23 .position').css("color", "black");
+  $('.box23 .name').css("color", "white");
+  $('.box23 .position').css("color", "white");
 }
 var change24 = function(){
   $('.box24 .el').css("opacity", "0.0");
@@ -398,8 +398,8 @@ var revert24 = function(){
   $('.box24 .el').css("opacity", "1.0");
   $('.box24').css("background", "none");
   $('.box24 .number').css("opacity", "1.0");
-  $('.box24 .name').css("color", "black");
-  $('.box24 .position').css("color", "black");
+  $('.box24 .name').css("color", "white");
+  $('.box24 .position').css("color", "white");
 }
 var change25 = function(){
   $('.box25 .el').css("opacity", "0.0");
@@ -413,8 +413,8 @@ var revert25 = function(){
   $('.box25 .el').css("opacity", "1.0");
   $('.box25').css("background", "none");
   $('.box25 .number').css("opacity", "1.0");
-  $('.box25 .name').css("color", "black");
-  $('.box25 .position').css("color", "black");
+  $('.box25 .name').css("color", "white");
+  $('.box25 .position').css("color", "white");
 }
 var change26 = function(){
   $('.box26 .el').css("opacity", "0.0");
@@ -428,8 +428,8 @@ var revert26 = function(){
   $('.box26 .el').css("opacity", "1.0");
   $('.box26').css("background", "none");
   $('.box26 .number').css("opacity", "1.0");
-  $('.box26 .name').css("color", "black");
-  $('.box26 .position').css("color", "black");
+  $('.box26 .name').css("color", "white");
+  $('.box26 .position').css("color", "white");
 }
 var change27 = function(){
   $('.box27 .el').css("opacity", "0.0");
@@ -443,8 +443,8 @@ var revert27 = function(){
   $('.box27 .el').css("opacity", "1.0");
   $('.box27').css("background", "none");
   $('.box27 .number').css("opacity", "1.0");
-  $('.box27 .name').css("color", "black");
-  $('.box27 .position').css("color", "black");
+  $('.box27 .name').css("color", "white");
+  $('.box27 .position').css("color", "white");
 }
 var change28 = function(){
   $('.box28 .el').css("opacity", "0.0");
@@ -458,8 +458,8 @@ var revert28 = function(){
   $('.box28 .el').css("opacity", "1.0");
   $('.box28').css("background", "none");
   $('.box28 .number').css("opacity", "1.0");
-  $('.box28 .name').css("color", "black");
-  $('.box28 .position').css("color", "black");
+  $('.box28 .name').css("color", "white");
+  $('.box28 .position').css("color", "white");
 }
 var change29 = function(){
   $('.box29 .el').css("opacity", "0.0");
@@ -473,8 +473,8 @@ var revert29 = function(){
   $('.box29 .el').css("opacity", "1.0");
   $('.box29').css("background", "none");
   $('.box29 .number').css("opacity", "1.0");
-  $('.box29 .name').css("color", "black");
-  $('.box29 .position').css("color", "black");
+  $('.box29 .name').css("color", "white");
+  $('.box29 .position').css("color", "white");
 }
 var change30 = function(){
   $('.box30 .el').css("opacity", "0.0");
@@ -488,6 +488,6 @@ var revert30 = function(){
   $('.box30 .el').css("opacity", "1.0");
   $('.box30').css("background", "none");
   $('.box30 .number').css("opacity", "1.0");
-  $('.box30 .name').css("color", "black");
-  $('.box30 .position').css("color", "black");
+  $('.box30 .name').css("color", "white");
+  $('.box30 .position').css("color", "white");
 }
